08ce81d3c9b6abfbacd92edb2dac4394e8eac35f
I__YaNG <372907906@qq.com>
2016-02-23 23:18:21 +0800

    更新部分UI

--- app/src/main/java/com/thirdnet/echat/activity/ConversationActivity.java
+++ app/src/main/java/com/thirdnet/echat/activity/ConversationActivity.java
@@ -2,14 +2,39 @@ package com.thirdnet.echat.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
 
 import com.thirdnet.echat.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class ConversationActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation);
+        ButterKnife.bind(this);
+        mToolbar.setNavigationIcon(R.drawable.ic_menu_back);
+        setSupportActionBar(mToolbar);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_conversation, menu);
+        return true;
     }
 }
